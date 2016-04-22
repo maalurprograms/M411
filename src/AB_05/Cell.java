d5bfbdbf6740330c522507faa1e29d0d431c8193
Jonas Cosandey <jonascosandey@hotmail.com>
2016-04-22 15:34:17 +0200

    lol

--- src/AB_05/Cell.java
+++ src/AB_05/Cell.java
@@ -2,23 +2,15 @@ package AB_05;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-/**
- * Created by jonas on 31.03.16.
- */
-public class Cell extends JButton {
-    public boolean alive = false;
-    public int aliveInRange = 0;
+class Cell extends JButton {
+    boolean alive = false;
+    int aliveInRange = 0;
 
-    public Cell() {
-        addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                alive = !alive;
-                if (alive){setBackground(Color.YELLOW);}else {setBackground(Color.BLACK);}
-            }
+    Cell() {
+        addActionListener(e -> {
+            alive = !alive;
+            if (alive){setBackground(Color.YELLOW);}else {setBackground(Color.BLACK);}
         });
     }
 }
--- src/AB_05/GameOfLife.java
+++ src/AB_05/GameOfLife.java
@@ -1,29 +1,22 @@
 package AB_05;
 
-import jdk.nashorn.internal.scripts.JO;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-/**
- * Created by jonas on 31.03.16.
- */
-public class GameOfLife {
+class GameOfLife {
 
     private Cell[][] cells;
-    public int updateTime;
+    private int updateTime;
 
-    private JFrame frame = new JFrame();
     private int[][] range = new int[][] {{-1, 1}, {0, 1},{1, 1},{-1, 0},{1, -1},{-1, -1},{0, -1},{1, 0}};
 
-    public GameOfLife(int updateTime, int fieldSize) {
+    GameOfLife(int updateTime, int fieldSize) {
         this.updateTime = updateTime;
         cells = new Cell[fieldSize][fieldSize];
 
+        JFrame frame = new JFrame();
         frame.setSize(1000, 1000);
         frame.setLocation(java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width/2,java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height/2);
         frame.setLayout(new GridLayout(cells.length, cells.length));
@@ -38,11 +31,11 @@ public class GameOfLife {
         mainLoop();
     }
 
-    public void mainLoop(){
+    private void mainLoop(){
         while(true){
             countAlive();
             checkCells();
-            try {Thread.sleep(updateTime);}catch (Exception e){}
+            try {Thread.sleep(updateTime);}catch (Exception ignored){}
         }
     }
 
@@ -63,17 +56,17 @@ public class GameOfLife {
     }
 
     private void checkCells(){
-        for (int i = 0; i<cells.length;i++) {
-            for (int j = 0; j<cells[i].length;j++) {
-                if(cells[i][j].alive) {
-                    if (cells[i][j].aliveInRange < 2 || cells[i][j].aliveInRange > 3) {
-                        cells[i][j].alive = false;
-                        cells[i][j].setBackground(Color.BLACK);
+        for (Cell[] cell : cells) {
+            for (Cell aCell : cell) {
+                if (aCell.alive) {
+                    if (aCell.aliveInRange < 2 || aCell.aliveInRange > 3) {
+                        aCell.alive = false;
+                        aCell.setBackground(Color.BLACK);
                     }
                 } else {
-                    if (cells[i][j].aliveInRange == 3){
-                        cells[i][j].alive = true;
-                        cells[i][j].setBackground(Color.YELLOW);
+                    if (aCell.aliveInRange == 3) {
+                        aCell.alive = true;
+                        aCell.setBackground(Color.YELLOW);
                     }
                 }
             }

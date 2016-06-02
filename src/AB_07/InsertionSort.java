68033b3dd4b449360cfa25bc06f7780bb1233fbf
Jonas Cosandey <jonascosandey@hotmail.com>
2016-06-02 11:27:17 +0200

    lol bugs very wierd much confuse

--- src/AB_07/InsertionSort.java
+++ src/AB_07/InsertionSort.java
@@ -4,9 +4,24 @@ package AB_07;
  * Created by jonas on 26.05.16.
  */
 public class InsertionSort {
-    public static void main(String[] args){
-        int[] sort_array = GenerateSortList.makeSortArray();
+    public static void main(String[] args) {
+        int[] liste = GenerateSortList.makeSortArray();
+        for (int i : insertionsort(liste)) {
+            System.out.println(i);
+        }
+    }
 
-
+    public static int[] insertionsort(int[] liste) {
+        int tempList;
+        for (int i = 1; i < liste.length; i++) {
+            tempList = liste[i];
+            int x = i;
+            while (x > 0 && liste[x - 1] > tempList) {
+                liste[x] = liste[x - 1];
+                x--;
+            }
+            liste[x] = tempList;
+        }
+        return liste;
     }
 }
